e7e6cda73e415d2dfe5c23d43d3520d99b361076
marcusWittho <witthoeft.mcs@hotmail.com>
2020-11-26 18:41:45 -0300

    Resolução do exercício 1 do dia 2

---
+++ Introdução à JavaScript e Lógica de Programação/dia_2/script.js
@@ -0,0 +1,9 @@
+// Array utilizado nos exercícios do 1 ao 7
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Exercício 1
+for (number of numbers) {
+  console.log(number);
+}
+
+//=================================================
