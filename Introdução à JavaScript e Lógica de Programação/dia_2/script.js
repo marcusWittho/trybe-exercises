4f07633f16e64706787039c09a438a845bb9ecff
marcusWittho <witthoeft.mcs@hotmail.com>
2020-11-26 18:55:05 -0300

    Resolução do exercício 2 do dia 2

--- Introdução à JavaScript e Lógica de Programação/dia_2/script.js
+++ Introdução à JavaScript e Lógica de Programação/dia_2/script.js
@@ -1,9 +1,16 @@
 // Array utilizado nos exercícios do 1 ao 7
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-// Exercício 1
-for (number of numbers) {
-  console.log(number);
-}
+// Exercício 1 (Exibe o conteúdo do array)
+// for (number of numbers) {
+//   console.log(number);
+// }
 
 //=================================================
+
+// Exercício 2 (Soma todos os itens do array)
+let total = numbers.reduce((total, number) => total + number, 0);
+console.log(total);
+
+//=================================================
+
