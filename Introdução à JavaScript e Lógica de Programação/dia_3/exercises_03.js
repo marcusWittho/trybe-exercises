4d09aea516b0f02c22d751091cf8c950e1ab3c8f
marcusWittho <witthoeft.mcs@hotmail.com>
2020-11-27 19:22:40 -0300

    Resolução do exercício 3 do dia 3

---
+++ Introdução à JavaScript e Lógica de Programação/dia_3/exercises_03.js
@@ -0,0 +1,19 @@
+function inverseTriangle(n) {
+  let star = '';
+  let count = n;
+
+  for (let row = 0; row < n; row += 1) {
+    for (let column = 0; column <= n; column += 1) {
+      if (column < count) {
+        star += ' ';
+      }else {
+        star += '*';
+      }
+    }
+    console.log(star);
+    star = '';
+    count -= 1;    
+  }
+  
+};
+inverseTriangle(5);
