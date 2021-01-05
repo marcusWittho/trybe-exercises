ad59ee714c135b033bac1bb5c4f3ce805a3810f3
marcusWittho <witthoeft.mcs@hotmail.com>
2021-01-05 11:00:47 -0300

    Exercício 1 finalizado

--- Introdução à JavaScript e Lógica de Programação/dia_1/exercise_08.js
+++
@@ -1,9 +0,0 @@
-let num_1 = 1;
-let num_2 = 2;
-let num_3 = 3;
-
-if (num_1 % 2 == 0 || num_2 % 2 == 0 || num_3 % 2 == 0) {
-  console.log(true);
-}else {
-  console.log(false);
-};
--- Introdução à JavaScript e Lógica de Programação/dia_3/exercise_02.js
+++
@@ -1,9 +0,0 @@
-function triangle(n) {
-  let star = '';
-  for (let i = 0; i < n; i += 1) {
-    star += '*';
-    console.log(star);
-  }
-  return star;
-};
-triangle(5);
