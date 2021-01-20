2efffd091ecc77dafcf9ed8daf8461b3ee34854d
marcusWittho <witthoeft.mcs@hotmail.com>
2021-01-20 15:26:05 -0300

    Dia-3 resolução do exercício 1

---
+++ Bloco_08/dia_3/exercise_1.js
@@ -0,0 +1,18 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+  const newArray = [];
+  return arrays.reduce((acumulator, array) => acumulator.concat(array), newArray);
+
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
