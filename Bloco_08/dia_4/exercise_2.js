ad3978f3fac58eb87617726caee85d3d2239920a
marcusWittho <witthoeft.mcs@hotmail.com>
2021-01-21 19:37:34 -0300

    Dia-4 resolução do exercício 2

--- Bloco_08/dia_4/exercise_2.js
+++ Bloco_08/dia_4/exercise_2.js
@@ -2,6 +2,10 @@ const assert = require('assert')
 
 // escreva sum abaixo
 
+const sum = (...params) => {
+  return params.reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+}
+
 assert.strictEqual(sum(), 0)
 assert.strictEqual(sum(1), 1)
 assert.strictEqual(sum(1, 2), 3)
