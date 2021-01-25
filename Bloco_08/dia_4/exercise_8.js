4143e7c523f642f6fb987cdd71be4bb6e27825f2
marcusWittho <witthoeft.mcs@hotmail.com>
2021-01-24 21:13:08 -0300

    Resolução do requisito 4

--- Bloco_08/dia_4/exercise_8.js
+++ Bloco_08/dia_4/exercise_8.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 
 // escreva greet abaixo
 
+
 assert.strictEqual(greet("John"), "Hi John")
 assert.strictEqual(greet("John", "Good morning"), "Good morning John")
 assert.strictEqual(greet("Isabela", "Oi"), "Oi Isabela")
